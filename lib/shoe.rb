dfd64d6fb975e9bc2e9674d252c2ea9e21de7e67
George Talnagi <gtalnagi@yahoo.com>
2020-08-04 21:21:58 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,10 @@
-# Make your shoe class here!
+class Shoe
+  attr_accessor :brand, :color, :size, :material, :condition 
+  attr_reader :title
+ 
+  def initialize(brand)
+    @brand = brand
+  end
+ 
+
+end
