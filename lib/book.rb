132a1749a5d44841e65972e857b2f09cfaf45249
George Talnagi <gtalnagi@yahoo.com>
2020-08-04 21:09:54 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,21 +1,21 @@
-class Book 
+class Book
+ 
   def initialize(title)
-    @title = title 
-  end 
-  
-  def title 
-    @title 
-  end 
-
+    @title = title
+  end
+ 
+  def title
+    @title
+  end
+ 
   def author=(author)
     @author = author
   end
-  
+ 
   def author
     @author
   end
-
-end 
-
+ 
+end
 
 Book.new("And Then There Were None")
